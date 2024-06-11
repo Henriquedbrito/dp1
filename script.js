ea81e895d535d676dec374c5a2b64c908928238e
Henriquedbrito <rm98831@fiap.com.br>
2024-06-11 11:07:54 -0300

    Exicício_dois

--- script.js
+++ script.js
@@ -7,4 +7,50 @@ console.log(valorIndefinido);
 console.log(valorVazio);
 console.log(valorPreenchido);
 
+//Exercício - 2
+let numeroUm = 1;
 
+// Utilizamos o "!=" para comparar se um valor é diferente de outro
+if (numeroUm != 2){
+    console.log("É um valor diferente de 2");
+}else {
+console.log("O valor não é diferente de 2");
+}
+// Utilizamos o "==" para comparar se um valor é igual o outro
+if (numeroUm == 1){
+    console.log("O valor è igual a 1");
+}else{
+    console.log("O valor não é igual a 1");
+}
+// Utilizamos o ">=" para comparar se um valor é maior ou igual o outro
+ if (numeroUm >= 1){
+    console.log("o valor é maior ou igual á 1");
+}else{
+    console.log("o valor não é maior ou igual a 1");
+}
+
+//Exercício - 3
+let abaixoDoPeso = 18;
+let pesoIdeal = 21;
+let acimaDoPeso = 25
+
+function IMC(peso){
+    if (peso < 18.5){
+        console.log("abaixo do peso");
+    }else if (peso =< 24.9){
+        console.log("peso ideal");
+    }else {
+        console.log("acima do peso");
+    }
+}
+
+IMC(abaixoDoPeso);
+IMC(pesoIdeal);
+IMC(acimaDoPeso);
+
+//Exercício - 4
+//Exercício - 5
+//Exercício - 6
+//Exercício - 7
+//Exercício - 8
+//Exercício - 9
